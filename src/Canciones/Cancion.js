767357247445d00d011a6af4eeac6c2de4319cef
Angel Valera Motos <angelvalera.epal@gmail.com>
2020-11-08 22:18:04 +0100

    [HU2] #13 Avanzo #19 #37: Defino una nueva clase Cancion que se encarga de definir una sola cancion

---
+++ src/Canciones/Cancion.js
@@ -0,0 +1,34 @@
+// Clase que define una canción
+// `id` = identificador de una canción
+// `nombre` = nombre de la canción
+// `artista` = Artista o grupo que compuso la canción
+// `letra` = letra de la canción
+class Cancion {
+  constructor(id, nombre, grupo, letra) {
+    this.id = id;
+    this.nombre = nombre;
+    this.artista = artista;
+    this.letra = letra;
+  }
+
+  to_string() {
+    return this.id + ", " + this.nombre + ", " + this.Grupo + ", " + this.letra;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getNombre() {
+    return this.nombre;
+  }
+
+  getGrupo() {
+    return this.grupo;
+  }
+
+  getLetra() {
+    return this.letra;
+  }
+}
+module.exports = Cancion;

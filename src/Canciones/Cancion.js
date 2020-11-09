396e70fe211d6ba750dbd7213b5606d9a0b50fb6
Angel Valera Motos <angelvalera.epal@gmail.com>
2020-11-09 08:09:18 +0100

    [HU2] #13 Avanzo #19: Corrijo la clase CancionController tras testearla, Ahora podemos hacer concultas sobre canciones

--- src/Canciones/Cancion.js
+++ src/Canciones/Cancion.js
@@ -1,19 +1,19 @@
 // Clase que define una canción
 // `id` = identificador de una canción
 // `nombre` = nombre de la canción
-// `artista` = Artista o grupo que compuso la canción
+// `Grupo` = Grupo o grupo que compuso la canción
 // `letra` = letra de la canción
 class Cancion {
   constructor(id, nombre, grupo, letra) {
     this.id = id;
     this.nombre = nombre;
-    this.artista = artista;
+    this.Grupo = grupo;
     this.letra = letra;
   }
 
   to_string() {
     return this.id + ", " + this.nombre + ", " + this.Grupo + ", " + this.letra;
-  }
+  } 
 
   getId() {
     return this.id;

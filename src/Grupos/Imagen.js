d775612a6b14457299a47a9e8af836bd64378014
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-01-14 20:23:07 +0100

    Avanzo #18: Agrego comprobacion de URL
    Compruebo que la url introducida sea una url valida

--- src/Grupos/Imagen.js
+++ src/Grupos/Imagen.js
@@ -1,21 +1,25 @@
 // Clase que define a un artista o grupo de música
 class Imagen {
-  constructor(id, label, url_img) {    
+  constructor(id, label, url_img) {
     if (this.checkTypes(id, label, url_img)) {
       this.id = id;
       this.label = label;
-      this.url_img = url_img;
+      if (this.validateUrl(url_img)) {
+        this.url_img = url_img;
+      } else {
+        throw "No se ha podido crear la imagen: Url incorrecta";
+      }      
     } else {
       throw "No se ha podido crear la imagen: Parámetros incorrectos";
-    }    
+    }
   }
 
-  checkTypes(id, label, url_img) {    
+  checkTypes(id, label, url_img) {
     return (
       typeof id === "number" &&
       typeof label === "string" &&
       typeof url_img === "string"
-    );           
+    );
   }
 
   to_string() {
@@ -33,9 +37,19 @@ class Imagen {
   getUrl_img() {
     return this.url_img;
   }
-  
-};
+
+  validateUrl(url) {
+    var pattern = new RegExp(
+      "^(https?:\\/\\/)?" + // protocol
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+        "(\\#[-a-z\\d_]*)?$",
+      "i"
+    ); // fragment locator
+    return !!pattern.test(url);
+  }
+}
 
 module.exports = Imagen;
-
-

9b5285e747e90c5ca12e6c6e541343d9a727b205
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-01-14 20:32:52 +0100

    Avanzo #21: Agrego comprobacion de tipos a los parametros
    Compruebo los tipos de la entidad Imagen y lanzo la excepcion correspondiente en caso de que falle

--- src/Grupos/RedSocial.js
+++ src/Grupos/RedSocial.js
@@ -1,16 +1,26 @@
 // Clase que define a un artista o grupo de música
-// `id` = identificador de la imagen
-// `nombre` =  nombre de la red social
-// `URL` =  url de la red social donde se encuentra la imagen
 class RedSocial {
   constructor(id, nombre, URL) {
-    this.id = id;
-    this.nombre = nombre;
     this.URL = URL;
+    if (this.checkTypes(id, nombre, URL)) {
+      this.id = id;
+      this.nombre = nombre;
+      this.URL = URL;
+    } else {
+      throw "No se ha podido crear la red Social: Parámetros incorrectos";
+    }
+  }
+
+  checkTypes(id, nombre, URL) {
+    return (
+      typeof id === "number" &&
+      typeof label === "string" &&
+      typeof url_img === "string"
+    );
   }
 
   to_string() {
-    return this.id + ", " + this.nombre + ", " + this.URL;
+    return ` ${this.id} , ${this.label} , ${this.URL}`;
   }
 
   getId() {

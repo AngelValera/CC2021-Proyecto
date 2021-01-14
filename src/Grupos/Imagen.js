3ef284d16b107fe75e212442b878403072ce8a1b
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-01-14 20:21:34 +0100

    Avanzo #18: Agrego comprobacion de tipos a los parametros
    Compruebo los tipos de la entidad Imagen y lanzo la excepcion correspondiente en caso de que falle

--- src/Grupos/Imagen.js
+++ src/Grupos/Imagen.js
@@ -1,16 +1,25 @@
 // Clase que define a un artista o grupo de música
-// `id` = identificador de la imagen
-// `label` =  label de la imagen
-// `url_img` =  url del lugar donde se encuentra la imagen
 class Imagen {
-  constructor(id, label, url_img) {
-    this.id = id;
-    this.label = label;
-    this.url_img = url_img;
+  constructor(id, label, url_img) {    
+    if (this.checkTypes(id, label, url_img)) {
+      this.id = id;
+      this.label = label;
+      this.url_img = url_img;
+    } else {
+      throw "No se ha podido crear la imagen: Parámetros incorrectos";
+    }    
+  }
+
+  checkTypes(id, label, url_img) {    
+    return (
+      typeof id === "number" &&
+      typeof label === "string" &&
+      typeof url_img === "string"
+    );           
   }
 
   to_string() {
-    return this.id + ", " + this.label + ", " + this.URL;
+    return ` ${this.id} , ${this.label} , ${this.url_img}`;
   }
 
   getId() {
@@ -24,5 +33,9 @@ class Imagen {
   getUrl_img() {
     return this.url_img;
   }
-}
+  
+};
+
 module.exports = Imagen;
+
+

c4d14d321b2a112fd5fbaf14b92b29368c9942ab
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-01-17 18:24:35 +0100

    [HU10]#66: Elimino requires que no se han utilizado

--- src/routes/grupos.js
+++ src/routes/grupos.js
@@ -1,8 +1,6 @@
 const { Router } = require("express");
-const Grupo = require("../Grupos/Grupo.js");
 const GrupoController = require("../Grupos/GrupoController.js");
-const Imagen = require('../Grupos/Imagen.js');
-const RedSocial = require("../Grupos/RedSocial.js");
+
 
 const router = Router();
 let grupoController = new GrupoController();

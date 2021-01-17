ecaa2368fdccb857c9879456ab97c21d6baa0af0
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-01-17 17:00:22 +0100

    [HU1]#12: Agrego rutas para servir todos los grupos
    Agrego una ruta para que se puedan obtener todos los grupos registrados en el sistema

--- src/routes/grupos.js
+++ src/routes/grupos.js
@@ -7,7 +7,12 @@ const RedSocial = require("../Grupos/RedSocial.js");
 const router = Router();
 let grupoController = new GrupoController();
 
-
+router.get("/grupos", (req, res) => {
+  let data = grupoController.getGroups();
+  res.status(200);
+  res.header("Content-Type", "application/json");
+  res.json(data);
+});
 
 
 module.exports = router;

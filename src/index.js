ad46b78d36d0ad2e4c5d325ebcac18ca582e606d
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-01-17 20:07:05 +0100

    Avanzo #62: Configuro el servicio de configuracion distribuida Etcd3

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+const { Etcd3 } = require("etcd3");
+const client = new Etcd3();
+
+async function getPort() {
+  const port = await client.get("LyricsHunterPort");
+  return port;
+}
+
 // Settings
-app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
 
 // Middlerwares
@@ -14,9 +21,14 @@ app.use(express.json());
 // Routes
 app.use("/api",require("./routes/grupos"));
 
-// Starting the server
-app.listen(app.get('port'), () => {
-    console.log(`Server on Port ${app.get("port")}`);
+//Starting the server
+(async () => {    
+  let PORT = await getPort();  
+  app.set("port", PORT || process.env.PORT || 3000);   
+})().then(() => {
+  app.listen(app.get("port"), () => {
+    console.log(`Server on port ${app.get("port")}`);
+  });
 });
 
 module.exports = app;

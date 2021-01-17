8954970ddedb5c903a2c16dc1cee5e6561724fd1
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-01-17 16:44:25 +0100

    [HU10]#66 Avanzo#16: Modifico el metodo para agregar nuevos grupos
    Considero que es mejor pasarle todos los parametros y que se cree el grupo con imagenes y redes sociales internamente que estar haciendolo siempre antes de agregar un nuevo grupo

--- src/Grupos/GrupoController.js
+++ src/Grupos/GrupoController.js
@@ -1,4 +1,7 @@
 const Grupo = require("./Grupo.js");
+const Imagen = require("./Imagen");
+const RedSocial = require("./RedSocial");
+const _ = require('underscore');
 
 class GrupoController {
   constructor() {
@@ -6,16 +9,65 @@ class GrupoController {
   }
 
   // Agregamos un nuevo grupo
-  addNewGroup(nuevoGrupo) {
-    if (nuevoGrupo instanceof Grupo) {
-      if (!(nuevoGrupo.getId() in this.grupos)) {
-        this.grupos[nuevoGrupo.getId()] = nuevoGrupo;
-      } else {
-        throw "No se ha podido agregar el grupo: El grupo ya existe";        
-      }      
+  addNewGroup(
+    nombre,
+    anioFormacion,
+    anioSeparacion,
+    estilo,
+    genero,
+    web,
+    redesSociales,
+    biografia,
+    miembros,
+    pais,
+    imagenes
+  ) {
+    _.each(this.grupos, (grupo, i) => {      
+      if(grupo.getNombre() == nombre ){
+        throw "No se ha podido agregar el grupo: El grupo ya existe";
+      }
+    });
+
+    let imgs = [];
+    if (Array.isArray(imagenes)) {      
+      _.each(imagenes, (img,i) => {
+        try {
+          imgs.push(new Imagen(i, img.label, img.url_img)); 
+        } catch (error) {
+          throw "No se pudo crear la imagen";
+        }        
+      });      
     } else {
-      throw "No se ha podido agregar el grupo: Parámetros incorrectos";
+      throw "Tipo Incorrecto: Imagenes no es un array";
     }
+    let rrss = [];
+    if (Array.isArray(redesSociales)) {
+      _.each(redesSociales, (red, i) => {
+        try {
+          rrss.push(new RedSocial(i, red.nombre, red.url));
+        } catch (error) {
+          throw "No se pudo crear la red Social";
+        }
+      });
+    } else {
+      throw "Tipo Incorrecto: redesSociales no es un array";
+    }
+    let id = Object.keys(this.grupos).length;    
+    let nuevoGrupo = new Grupo(
+      id,
+      nombre,
+      anioFormacion,
+      anioSeparacion,
+      estilo,
+      genero,
+      web,
+      rrss,
+      biografia,
+      miembros,
+      pais,
+      imgs
+    );
+    this.grupos[id] = nuevoGrupo; 
   }
 
   // buscamos el grupo por su nombre
@@ -26,12 +78,12 @@ class GrupoController {
     if (grupoBuscado == undefined) {
       throw "El grupo seleccionado no se ha encontrado.";
     }
-    return this.grupos[grupoBuscado];    
+    return this.grupos[grupoBuscado];
   }
 
   // Obtenemos todos los grupos
   getGroups(){
-   return this.grupos; 
+    return this.grupos;
   }
 }
 

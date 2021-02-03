594d7fd564f89b46c4ca75c816c1c89d9337a5a4
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-02-03 21:02:12 +0100

    Avanzo #69: Agrego rutas de usuarios al microservicio

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ app.use(express.json());
 // Routes
 app.use(require("./routes/grupos"));
 app.use(require("./routes/canciones"));
+app.use(require("./routes/usuarios"));
 
 // Starting the server
 (async () => {

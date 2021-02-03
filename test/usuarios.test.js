e9eb11f04820a77bed39e1ad3a767244a4b03277
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-02-03 20:54:43 +0100

    [HU13] #75: Agrego test para obtener todos los usuarios

--- test/usuarios.test.js
+++ test/usuarios.test.js
@@ -8,17 +8,17 @@ describe("Realizar test a los Usuarios", function () {
   let controlador = new UsuarioController();
 
   it("Los usuarios deben crearse correctamente", function () {
-    controlador.addNewUser(
-      "Angel",
-      "angel@prueba.com",
-      "123456",
-      1      
-    );
+    controlador.addNewUser("Angel", "angel@prueba.com", "123456", 1);
     assert.equal(
       controlador.getUserByName("Angel").to_string(),
       `0, Angel, angel@prueba.com, 123456, 1`,
       "Correcto"
     );
+  });
+
+  it("Los usuarios deben obtenerse correctamente", function () {
+    controlador.addNewUser("Jose", "jose@prueba.com", "654321", 0);
+    assert.equal(Object.keys(controlador.getUsers()).length, 2, "Correcto");
   });
 
 

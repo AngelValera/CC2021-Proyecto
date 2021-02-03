4b852fd58a34acd2e6d41563ef6b6e936c0fda31
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-02-03 13:37:36 +0100

    [HU11] #73: Agrego test para comprobar que las canciones se crean correctamente

--- test/canciones.test.js
+++ test/canciones.test.js
@@ -5,28 +5,33 @@ var expect = chai.expect();
 const Cancion = require("../src/Canciones/Cancion");
 const CancionController = require("../src/Canciones/CancionController");
 
-describe("Crear una cancion", function () {
+describe("Realizar test a las Canciones de música", function () {
+  let controlador = new CancionController();
+
   it("Las canciones deben crearse correctamente", function () {
-    var nueva_cancion = new Cancion("1", "nombre", "grupo", "letra");
+    controlador.addNewSong(
+      "Faint",
+      "Linkin Park",
+      `I am a little bit of loneliness a little bit of disregard \
+        Handful of complaints but I can't help the fact that everyone can see \
+        these scars \
+        I am what I want you to want what I want you to feel \
+        But it's like no matter what I do, I can't convince you, to just believe \
+        this is real`
+    );
     assert.equal(
-      nueva_cancion.to_string(),
-      "1, nombre, grupo, letra",
+      controlador.getSongByName("Faint")[0].to_string(),
+      `0, Faint, Linkin Park, I am a little bit of loneliness a little bit of disregard \
+        Handful of complaints but I can't help the fact that everyone can see \
+        these scars \
+        I am what I want you to want what I want you to feel \
+        But it's like no matter what I do, I can't convince you, to just believe \
+        this is real`,
       "Correcto"
     );
   });
+
+
+
 });
 
-describe("Consultar una cancion", function () {
-  it("Las canciones deben consultarse correctamente", function () {
-    var controlador = new CancionController();
-    controlador.addCancion("1", "nombre2", "grupo", "letra");
-    controlador.addCancion("2", "nombre2", "grupo2", "letra2");
-
-    assert.equal(
-      controlador.getCancion("nombre2","grupo"),
-      "1, nombre2, grupo, letra",
-      "Correcto"
-    ); 
-    
-  });
-});

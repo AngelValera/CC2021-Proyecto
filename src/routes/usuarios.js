c44fbfafb9c758a7491c084c2b24d15eb2ea828c
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-02-03 21:04:15 +0100

    [HU13] #75: Agrego ruta para obtener un  usuario por nombre

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -11,7 +11,18 @@ router.get("/usuarios", (req, res) => {
   res.json(data);
 });
 
-
+router.get("/usuarios/:nombre", (req, res) => {
+  try {
+    let data = usuarioController.getUserByName(req.params.nombre);
+    res.status(200);
+    res.header("Content-Type", "application/json");
+    res.json(data);
+  } catch (exception) {
+    res.status(404);
+    res.header("Content-Type", "application/json");
+    res.json({ Error: exception });
+  }
+});
 
 
 module.exports = router;

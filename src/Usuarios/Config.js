9587d6d4063234f15eebc0edcc1603830495c889
Angel Valera Motos <angelvalera.epal@gmail.com>
2021-02-04 18:28:50 +0100

    Avanzo #69: Agrego configuracion del microservicio de usuarios
    [Skip ci]

---
+++ src/Usuarios/Config.js
@@ -0,0 +1,25 @@
+const { config } = require("dotenv").config();
+
+class Config {
+  constructor() {
+    var self = this;    
+    
+    self.assign_default_ip = () => {
+      self.ip =
+        process.env.LISTENING_IP_ADDRESS || "0.0.0.0";
+    };
+
+    self.assign_default_port = () => {
+      self.port = process.env.PORT || 3004;
+    }; 
+
+    self.assign_defaults = () => {
+      self.assign_default_ip();
+      self.assign_default_port();
+    };
+
+    self.assign_defaults();  
+  }
+}
+
+module.exports = { Config };
